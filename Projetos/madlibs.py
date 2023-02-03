3c3c8ea60d4398ebc49b0ce94be51e4ca76693d2
ShirleneSousa <103758882+ShirleneSousa@users.noreply.github.com>
2023-02-02 21:02:20 -0300

    Add files via upload

---
+++ Projetos/madlibs.py
@@ -0,0 +1,9 @@
+# “MadLibs tradicionais são feitos com papel e tinta. Mas agora que estamos na era digital, podemos recriar o jogo com algum código Python simples e reutilizar cada MadLib repetidamente.”#
+
+verbo = input("Verbo: ")
+adjetivo1 = input("adjetivo 1: ")
+adjetivo2 = input("adjetivo 2: ")
+
+madlibs = f"MadLibs tradicionais são {verbo} com papel e tinta. Mas agora que estamos na era {adjetivo1}, podemos recriar o jogo com algum código Python {adjetivo2} e reutilizar cada MadLib repetidamente."
+
+print(madlibs)
